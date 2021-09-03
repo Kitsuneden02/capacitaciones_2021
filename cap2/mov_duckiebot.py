190dd00056b099b31e7efb1ca7d9b51c432e66c5
Sebastián Herrera <sebastian.herrera.t@ug.uchile.cl>
2021-09-03 17:05:17 -0400

    ^/ ^\

--- cap2/mov_duckiebot.py
+++ cap2/mov_duckiebot.py
@@ -62,7 +62,20 @@ while True:
     # Esto es avanzar recto hacia adelante al apretar la tecla w
     if key == ord('w'):
         action = np.array([0.44, 0.0])
-
+    if key == ord('s'):
+        action = np.array([-0.44, 0.0])
+    if key == ord('d'):
+        action = np.array([0.0, -0.44])
+    if key == ord('a'):
+        action = np.array([0.0, 0.44])
+    if key == ord('q'):
+        action = np.array([0.35, +1])
+    if key == ord('e'):
+        action = np.array([0.35, -1])
+    if key == ord('z'):
+        action = np.array([-0.35, +1])
+    if key == ord('c'):
+        action = np.array([-0.35, -1])
     ### AGREGAR MÁS COMPORTAMIENTOS ###
 
 
