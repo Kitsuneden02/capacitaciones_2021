6738a994149401b3c2b96d4818d28594d866f4f1
Sebastián Herrera <sebastian.herrera.t@ug.uchile.cl>
2021-09-19 19:56:35 -0300

    Semana 4

--- cap3/det_pato.py
+++ cap3/det_pato.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     # Se leen los argumentos de entrada
     parser = argparse.ArgumentParser()
     parser.add_argument('--env-name', default="Duckietown-udem1-v1")
-    parser.add_argument('--map-name', default='udem1')
+    parser.add_argument('--map-name', default='mapatotalmenteoriginal')
     parser.add_argument('--distortion', default=False, action='store_true')
     parser.add_argument('--draw-curve', action='store_true', help='draw the lane following curve')
     parser.add_argument('--draw-bbox', action='store_true', help='draw collision detection bounding boxes')
@@ -60,8 +60,10 @@ if __name__ == '__main__':
 
     # Parametros para el detector de patos
     # Se debe encontrar el rango apropiado
-    lower_yellow = np.array([H_m, S_m, V_m])
-    upper_yellow = np.array([H_M, S_M, V_M])
+    lower_yellow = np.array([20, 220, 40])
+    upper_yellow = np.array([40, 255, 255])
+    lower_orange = np.array([10, 100, 20])
+    upper_orange = np.array([25, 255, 255])
     min_area = 2500
 
     while True:
@@ -87,14 +89,15 @@ if __name__ == '__main__':
         ### CÓDIGO DE DETECCIÓN POR COLOR ###
 
         #Transformar imagen a espacio HSV
-
+        img_in = cv2.cvtColor(obs,cv2.COLOR_RGB2HSV)
 
         # Filtrar colores de la imagen en el rango utilizando
-
-
+        mask1 = cv2.inRange(img_in, lower_yellow, upper_yellow)
+        mask2 = cv2.inRange(img_in, lower_orange, upper_orange)
+        mask = cv2.bitwise_or(mask1,mask2)
         # Bitwise-AND entre máscara (mask) y original (obs) para visualizar lo filtrado
 
-
+        img_out = cv2.bitwise_and(obs, obs, mask = mask)
         # Se define kernel para operaciones morfológicas
         kernel = np.ones((5,5),np.uint8)
 
@@ -102,28 +105,22 @@ if __name__ == '__main__':
         # Esto corresponde a hacer un Opening
         # https://docs.opencv.org/trunk/d9/d61/tutorial_py_morphological_ops.html
         #Operacion morfologica erode
-
+        img_out = cv2.erode(img_out, kernel, iterations = 3)
         #Operacion morfologica dilate
-
-
+        img_out = cv2.dilate(img_out, kernel, iterations = 2)
         # Busca contornos de blobs
         # https://docs.opencv.org/trunk/d3/d05/tutorial_py_table_of_contents_contours.html
-
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         # Iterar sobre contornos y dibujar bounding box de los patos
         for cnt in contours:
             # Obtener rectangulo que bordea un contorno
-
+            AREA = cv2.contourArea(cnt)
             #Filtrar por area minima
             if AREA > min_area: # DEFINIR AREA
+                x,y,w,h = cv2.boundingRect(cnt)
                 #Dibujar rectangulo en el frame original
-
-
-        # Se muestra en una ventana llamada "patos" la observación del simulador
-        # con los bounding boxes dibujados
-        cv2.imshow('patos', cv2.cvtColor(obs, cv2.COLOR_RGB2BGR))
-        # Se muestra en una ventana llamada "filtrado" la imagen filtrada
-        cv2.imshow('filtrado', image)
+                cv2.rectangle(obs, (x, y), (x+w, y+h), (255,0,0), 2)
 
 
     # Se cierra el environment y termina el programa
